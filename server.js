0f11a9baa05c5b8bda8fd288834b3568ad6e4be2
Ryan <rmurney@gmail.com>
2023-01-31 10:08:15 -0400

    catany update

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.post("/email", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Portfolio app listening at http://localhost:${port}`);
 });
