b7fc81e29b1530ff15693237e5a4ccc59f8273f7
Ryan Murney <rmurney@gmail.com>
2024-11-08 20:13:20 +0000

    TABS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,144 +1,144 @@
 function init() {
-    let margin = 100;
-    
-    if (window.screen.width <= 600) {
-        margin = 70;
-    } else if (window.screen.width > 992) {
-        document.getElementById('home').style.height = `${window.innerHeight - margin}px`;
-    }
+	let margin = 100;
+	
+	if (window.screen.width <= 600) {
+		margin = 70;
+	} else if (window.screen.width > 992) {
+		document.getElementById('home').style.height = `${window.innerHeight - margin}px`;
+	}
 
-    setTimeout(() => {
-        window.scrollTo({ 
-            top: 0
-        });
-    }, 200);
+	setTimeout(() => {
+		window.scrollTo({ 
+			top: 0
+		});
+	}, 200);
 }
 
 function moveToPanel(panel, isMobile) {
-    const element = document.getElementById(panel).offsetTop;
+	const element = document.getElementById(panel).offsetTop;
 
-    let margin = 100;
+	let margin = 100;
 
-    if (window.screen.width < 992) {
-        margin = 70;
-    }
+	if (window.screen.width < 992) {
+		margin = 70;
+	}
 
-    window.scrollTo({ 
-        top: element - margin,
-        behavior: 'smooth'
-    });
+	window.scrollTo({ 
+		top: element - margin,
+		behavior: 'smooth'
+	});
 
-    if (isMobile) {
-        showMobileMenu();
-    }
+	if (isMobile) {
+		showMobileMenu();
+	}
 }
 
 function showMobileMenu() {
-    const menu = document.getElementById('mobileMenu');
+	const menu = document.getElementById('mobileMenu');
 
-    if (menu.style.display == 'none') {
-        menu.style.display = 'flex';
-    } else {
-        menu.style.display = 'none';
-    }
+	if (menu.style.display == 'none') {
+		menu.style.display = 'flex';
+	} else {
+		menu.style.display = 'none';
+	}
 }
 
 function goToLink(url) {
-    if (url == 'git') {
-        url = 'https://github.com/Murnster';
-    } else if (url == 'linkedin') {
-        url = 'https://www.linkedin.com/in/ryanmurney/';
-    } else if (url == 'simplesport') {
-        url = 'https://murnster.github.io/SimpleSport/';
-    } else if (url == 'catan') {
-        url = 'https://murnster.github.io/Web-Catan-Board/';
-    }
+	if (url == 'git') {
+		url = 'https://github.com/Murnster';
+	} else if (url == 'linkedin') {
+		url = 'https://www.linkedin.com/in/ryanmurney/';
+	} else if (url == 'simplesport') {
+		url = 'https://murnster.github.io/SimpleSport/';
+	} else if (url == 'catan') {
+		url = 'https://murnster.github.io/Web-Catan-Board/';
+	}
 
-    window.open(url, '_blank');
+	window.open(url, '_blank');
 }
 
 function toast(success) {
-    let toast;
+	let toast;
 
-    if (success) {
-        toast = document.getElementById('successToast');
-    } else if (success === false) {
-        toast = document.getElementById('errorToast')
-    } else {
-        toast = document.getElementById('questionToast');
-    }
+	if (success) {
+		toast = document.getElementById('successToast');
+	} else if (success === false) {
+		toast = document.getElementById('errorToast')
+	} else {
+		toast = document.getElementById('questionToast');
+	}
 
-    toast.setAttribute('active', '');
+	toast.setAttribute('active', '');
 
-    const toastTimeout = setTimeout(() => {
-        toast.removeAttribute('active');
-    }, 7500);
+	const toastTimeout = setTimeout(() => {
+		toast.removeAttribute('active');
+	}, 7500);
 
-    toast.onclick = function() {
-        clearTimeout(toastTimeout);
-        toast.removeAttribute('active');
-    }
+	toast.onclick = function() {
+		clearTimeout(toastTimeout);
+		toast.removeAttribute('active');
+	}
 }
 
 async function sendEmail() {
-    const name = document.getElementById('contactName');
-    const email = document.getElementById('contactEmail');
-    const phone = document.getElementById('contactPhone');
-    const message = document.getElementById('contactMessage');
+	const name = document.getElementById('contactName');
+	const email = document.getElementById('contactEmail');
+	const phone = document.getElementById('contactPhone');
+	const message = document.getElementById('contactMessage');
 
-    if (name.value == '') {
-        name.focus();
-        toast(null);
-    } else if (email.value == '') {
-        email.focus();
-    } else if (phone.value == '') {
-        phone.focus();
-    } else if (message.value == '') {
-        message.focus();
-    } else {
-        if (cleanTrolls(name.value + ' ' + email.value + ' ' + phone.value + ' ' + message.value)) {
-            await fetch('/email', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: name.value,
-                    email: email.value,
-                    phone: phone.value,
-                    message: message.value
-                })
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                toast(true);
-            })
-            .catch((error) => {
-                console.error(error);
-                toast(false);
-            });
-        } else {
-            toast(null);
-        }
-    }
+	if (name.value == '') {
+		name.focus();
+		toast(null);
+	} else if (email.value == '') {
+		email.focus();
+	} else if (phone.value == '') {
+		phone.focus();
+	} else if (message.value == '') {
+		message.focus();
+	} else {
+		if (cleanTrolls(name.value + ' ' + email.value + ' ' + phone.value + ' ' + message.value)) {
+			await fetch('/email', {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json, text/plain, */*',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					name: name.value,
+					email: email.value,
+					phone: phone.value,
+					message: message.value
+				})
+			})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				toast(true);
+			})
+			.catch((error) => {
+				console.error(error);
+				toast(false);
+			});
+		} else {
+			toast(null);
+		}
+	}
 }
 
 function cleanTrolls(string) {
-    const words = string.split(' ');
-    var sendEmail = true;
-    
-    if (string.indexOf('aunt sum') != -1) {
-        sendEmail = false;
-    }
+	const words = string.split(' ');
+	var sendEmail = true;
+		
+	if (string.indexOf('aunt sum') != -1) {
+		sendEmail = false;
+	}
 
-    if (
-        words.indexOf('sum') != -1 ||
-        words.indexOf('bahd') != -1
-    ) {
-        sendEmail = false;
-    }
+	if (
+		words.indexOf('sum') != -1 ||
+		words.indexOf('bahd') != -1
+	) {
+		sendEmail = false;
+	}
 
-    return sendEmail;
+	return sendEmail;
 }
--- server.js
+++ server.js
@@ -11,33 +11,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/email", (req, res) => {
-  emailjs
-    .send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, req.body, {
-      publicKey: process.env.PUBLIC_KEY,
-      privateKey: process.env.PRIVATE_KEY,
-    })
-    .then(
-      function (response) {
-        console.log('SUCCESS!', response.status, response.text);
-        
-        res.status(200).send({
-          message: 'Email successfully sent'
-        });;
-      },
-      function (err) {
-        console.log('FAILED...', err);
-        
-        res.status(404).send({
-          message: 'Email failed to send'
-        });
-      },
-    );
+	emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, req.body, {
+		publicKey: process.env.PUBLIC_KEY,
+		privateKey: process.env.PRIVATE_KEY,
+	}).then(
+		function (response) {
+			console.log('SUCCESS!', response.status, response.text);
+			
+			res.status(200).send({
+				message: 'Email successfully sent'
+			});;
+		},
+		function (err) {
+			console.log('FAILED...', err);
+			
+			res.status(404).send({
+				message: 'Email failed to send'
+			});
+		},
+	);
 });
 
 app.listen(port, () => {
-  console.log(`Portfolio app listening at http://localhost:${port}`);
+	console.log(`Portfolio app listening at http://localhost:${port}`);
 });
